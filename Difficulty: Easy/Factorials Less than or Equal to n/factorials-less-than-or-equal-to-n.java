950a3769e59e45843cc0b8c8d69fdd0e36d3ffbe
Vedant Joshi <91942752+Vedantjoshi123@users.noreply.github.com>
2025-03-10 17:20:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Factorials Less than or Equal to n/factorials-less-than-or-equal-to-n.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long N = sc.nextLong();
+
+            Solution ob = new Solution();
+            ArrayList<Long> ans = ob.factorialNumbers(N);
+            for (long num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static ArrayList<Long> factorialNumbers(long n) {
+        long x = 1;
+        int i = 2;
+        ArrayList<Long> ans = new ArrayList<>();
+        while(x<=n){
+            ans.add(x);
+            x = x*i;
+            i++;
+        }
+        return ans;
+    }
+}
